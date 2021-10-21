859d4a47f7a3d10f14cf2647e2e0c7667c8fb47f
Sébastien Braun <sebbraun@gmail.com>
2021-10-21 18:03:50 +0200

    updated script to vault 1.8.3

--- ssh-automation/packer/files/script.sh
+++ ssh-automation/packer/files/script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 sudo su <<HERE
 apt-get install unzip
-curl https://releases.hashicorp.com/vault/1.3.0/vault_1.3.0_linux_amd64.zip --output /usr/local/bin/vault.zip
+curl https://releases.hashicorp.com/vault/1.8.4/vault_1.8.4_linux_amd64.zip --output /usr/local/bin/vault.zip
 unzip /usr/local/bin/vault.zip -d /usr/local/bin/
 mkdir -p /etc/vault/tls
 mv /tmp/ca.crt /etc/vault/tls/
